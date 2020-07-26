b59a73370ce3007f06e27191f2f81da5b93d2a64
Ramdhawa <shinlia21@gmail.com>
2020-07-26 11:12:00 +0800

    add tugas 3 js

---
+++ tugas3.js
@@ -0,0 +1,12 @@
+function eyo(x){
+  var ayo = console.log("Sebelum : ",x);
+  return ayo;
+}
+eyo("sebelum");
+function eya(x){
+  x = "sesudah"
+  y = x
+  var wow = console.log("Sesudah : ",y);
+  return wow;
+}
+eya("sebelum");
